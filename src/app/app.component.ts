2ccc9e6fe07492a7781d179f29d8595e36b44e94
Rodrigo S. Jauregui <rodrigo.jauregui@correo.ucu.edu.uy>
2023-06-19 13:11:52 -0300

    Routing working.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,11 @@ import { IonicModule } from '@ionic/angular';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Solicitudes', url: '/requests/requests', icon: 'construct' },
+    { title: 'Conexiones', url: '/connections', icon: 'people' },
+    { title: 'Perfil', url: '/profile/details', icon: 'person' },
+    { title: 'Habilidades', url: '/skills/list', icon: 'medal' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = [/*'Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'*/];
   constructor() {}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,149 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'authentication',
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./authentication/login/login.page').then( m => m.LoginPage)
+      },
+      {
+        path: 'registration',
+        children: [
+          {
+            path: 'start',
+            loadComponent: () => import('./authentication/registration/start/start.page').then( m => m.StartPage)
+          },
+          {
+            path: 'skills',
+            loadComponent: () => import('./authentication/registration/skills/skills.page').then( m => m.SkillsPage)
+          },
+          {
+            path: '',
+            redirectTo: 'start',
+            pathMatch: 'full'
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: 'requests',
+    children: [
+      {
+        path: 'requests',
+        loadComponent: () => import('./requests/requests/requests.page').then( m => m.RequestsPage)
+      },
+      {
+        path: 'request',
+        loadComponent: () => import('./requests/request/request.page').then( m => m.RequestPage)
+      },
+      {
+        path: 'create',
+        children: [
+          {
+            path: 'details',
+            loadComponent: () => import('./requests/create/details/details.page').then( m => m.DetailsPage)
+          },
+          {
+            path: 'skills',
+            loadComponent: () => import('./requests/create/skills/skills.page').then( m => m.SkillsPage)
+          },
+          {
+            path: '',
+            redirectTo: 'details',
+            pathMatch: 'full'
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: 'requests',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: 'connections',
+    children: [
+      {
+        path: 'search',
+        loadComponent: () => import('./connections/search/search.page').then( m => m.SearchPage)
+      },
+      {
+        path: '',
+        loadComponent: () => import('./connections/connections.page').then( m => m.ConnectionsPage)
+      },
+    ]
+  },
+  {
+    path: 'profile',
+    children: [
+      {
+        path: 'details',
+        loadComponent: () => import('./profile/details/details.page').then( m => m.DetailsPage)
+      },
+      {
+        path: 'modify',
+        loadComponent: () => import('./profile/modify/modify.page').then( m => m.ModifyPage)
+      },
+      {
+        path: 'password',
+        children: [
+          {
+            path: 'authorization',
+            loadComponent: () => import('./profile/password/authorization/authorization.page').then( m => m.AuthorizationPage)
+          },
+          {
+            path: 'change',
+            loadComponent: () => import('./profile/password/change/change.page').then( m => m.ChangePage)
+          },
+          {
+            path: '',
+            redirectTo: 'authorization',
+            pathMatch: 'full'
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: 'details',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: 'skills',
+    children: [
+      {
+        path: 'list',
+        loadComponent: () => import('./skills/list/list.page').then( m => m.ListPage)
+      },
+      {
+        path: 'modify',
+        loadComponent: () => import('./skills/modify/modify.page').then( m => m.ModifyPage)
+      },
+      {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: 'authentication',
+    pathMatch: 'full'
+  }
+]
+/*
+export const routes: Routes = [
+  {
     path: '',
     redirectTo: 'folder/inbox',
     pathMatch: 'full',
@@ -100,3 +243,4 @@ export const routes: Routes = [
     loadComponent: () => import('./skills/modify/modify.page').then( m => m.ModifyPage)
   },
 ];
+*/
