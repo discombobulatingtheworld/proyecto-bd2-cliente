8c4afd302b001245a2210e8b0d05bd777a114d14
Juan Lupi <juan.lupi@correo.ucu.edu.uy>
2023-06-29 14:39:40 -0300

    getHabilidadesCat

--- src/app/services/Habilidades/habilidades.service.ts
+++ src/app/services/Habilidades/habilidades.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Habilidad } from 'src/app/types/dtos/habilidad';
 import { Observable } from 'rxjs';
+import { HabilidadesCategoria } from 'src/app/types/dtos/habilidades-categoria';
 
 
 @Injectable({
@@ -15,6 +16,10 @@ export class HabilidadesService {
 
   getHabilidades(): Observable<Habilidad[]> {
     return this.http.get<Habilidad[]>('http://localhost:3001/api/habilidades');
+  }
+
+  getHabilidadesCategoria(): Observable<HabilidadesCategoria[]> {
+    return this.http.get<HabilidadesCategoria[]>('http://localhost:3001/api/habilidades/categorias');
   }
 }
 
