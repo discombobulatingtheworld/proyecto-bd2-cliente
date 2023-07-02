4f0754738f8de42c9e3a22c92b1333d2bb3a9540
tomaspasto24 <tomaspasto24@gmail.com>
2023-07-02 16:59:10 -0300

    conexiones ++

--- src/app/components/connections/active/active.component.ts
+++ src/app/components/connections/active/active.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { CONNECTIONS } from 'src/app/dummy/data';
+import { UsuariosService } from 'src/app/services/rest-api/usuarios.service';
 import { Conexion } from 'src/app/types/dtos/conexion';
 
 @Component({
@@ -12,16 +13,30 @@ import { Conexion } from 'src/app/types/dtos/conexion';
   styleUrls: ['./active.component.scss'],
   imports: [IonicModule, CommonModule, FormsModule],
 })
-export class ActiveComponent  implements OnInit {
+export class ActiveComponent implements OnInit {
   protected connections: Conexion[] = [];
 
-  constructor() { }
+  constructor(
+    private usuarioService: UsuariosService,
+  ) { }
 
   ngOnInit() {
-    this.connections = CONNECTIONS;
+    this.getConnections();
+  }
+
+  getConnections(): void {
+    this.usuarioService.getUserByToken().subscribe(({id}) => {
+      this.usuarioService.getConexionesUsuario(id).subscribe((connections) => {
+        this.connections = connections.filter((connection) => connection.aceptada);
+      })
+    });
   }
 
   protected onRemoveConnection(connection: Conexion): void {
-    // TODO: Implementar
+    this.usuarioService.getUserByToken().subscribe(({ id }) => {
+      this.usuarioService.removeConexionUsuario(id, connection.userId).subscribe((response) => {
+        this.getConnections();
+      })
+    });
   }
 }
--- src/app/services/rest-api/usuarios.service.ts
+++ src/app/services/rest-api/usuarios.service.ts
@@ -3,6 +3,7 @@ import { Usuario } from 'src/app/types/dtos/usuario';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Habilidad } from 'src/app/types/dtos/habilidad';
+import { Conexion } from 'src/app/types/dtos/conexion';
 
 @Injectable({
   providedIn: 'root'
@@ -27,7 +28,7 @@ export class UsuariosService {
     this.user = undefined;
     this.token = undefined;
   }
-  
+
   getUserByToken(): Observable<Usuario> {
     let token = sessionStorage.getItem('jwt');
 
@@ -36,6 +37,37 @@ export class UsuariosService {
     });
 
     return this.http.post<Usuario>('http://localhost:3001/api/autenticacion', {}, { headers });
+  }
+
+  getConexionesUsuario(id: number): Observable<Conexion[]> {
+    let token = sessionStorage.getItem('jwt');
+    const headers = { Authorization: `Bearer ${token}` };
+    return this.http.get<Conexion[]>('http://localhost:3001/api/usuarios/' + id + '/conexiones', { headers });
+  }
+
+  acceptConnectionToUser(idUsuario: number, idOtherUser: number): Observable<Conexion> {
+    let token = sessionStorage.getItem('jwt');
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer ' + token
+    });
+
+    return this.http.put<Conexion>(`http://localhost:3001/api/usuarios/${idUsuario}/conexiones/${idOtherUser}`, {}, { headers });
+  }
+
+  removeConexionUsuario(idUsuario: number, otherUserId: number): Observable<Conexion[]> {
+    let token = sessionStorage.getItem('jwt');
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer ' + token
+    });
+    return this.http.delete<any>(`http://localhost:3001/api/usuarios/${idUsuario}/conexiones/${otherUserId}`, { headers });
+  }
+
+  getSolicitudesUsuario(id: number): Observable<Conexion[]> {
+    let token = sessionStorage.getItem('jwt');
+    const headers = { Authorization: `Bearer ${token}` };
+    return this.http.get<Conexion[]>('http://localhost:3001/api/usuarios/' + id + '/conexiones', { headers });
   }
 
   getHabilidadesUsuario(id: number): Observable<Habilidad[]> {
@@ -62,6 +94,6 @@ export class UsuariosService {
     return this.http.post<any>(`http://localhost:3001/api/usuarios/${idUsuario}/habilidades`, {
       userId: idUsuario,
       skillId: idHabilidad
-    } ,{ headers });
+    }, { headers });
   }
 }
